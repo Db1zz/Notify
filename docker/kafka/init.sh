9ccd167cd15591952583f5018415e209ce601a42
Georgijs Onischenko <xel27@inbox.lv>
2026-05-11 15:05:58 +0200

    Make the  parameter optional for the consumer; if it is not specified, the notification service will not retry connecting to the metrics server.

--- docker/kafka/init.sh
+++ docker/kafka/init.sh
@@ -1,5 +1,19 @@
-/bin/kafka-topics --create \
+#!/bin/bash
+set -e
+
+echo "Waiting for Kafka..."
+
+until /bin/kafka-topics --bootstrap-server kafka:29092 --list >/dev/null 2>&1; do
+  sleep 2
+done
+
+echo "Creating topic..."
+
+/bin/kafka-topics --bootstrap-server kafka:29092 \
+  --create \
+  --if-not-exists \
   --topic user-notifs \
-  --bootstrap-server localhost:29092 \
   --partitions 1 \
   --replication-factor 1
+
+echo "Done."
